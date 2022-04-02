fe4c645e4671e49da273cb0015fff86f4cc7b6f9
高杰 <gao4433@qq.com>
2022-04-02 16:02:29 +0800

    SpringMVC组件解析

--- spring_mvc/src/main/java/cc/gaojie/controller/UserController.java
+++ spring_mvc/src/main/java/cc/gaojie/controller/UserController.java
@@ -2,6 +2,8 @@ package cc.gaojie.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.xml.ws.RequestWrapper;
 
@@ -13,12 +15,27 @@ import javax.xml.ws.RequestWrapper;
  */
 //放进Spring容器
 @Controller
+@RequestMapping("user")
 public class UserController {
 
-    //标注映射关系
-    @RequestMapping("/quick")
+    @RequestMapping(value = "/quick", method = RequestMethod.GET, params = {"username"})
     public String sava() {
         System.out.println("Controller save running……");
+        //在跳转时省略前缀和后缀
         return "success.jsp";
+    }
+
+    @RequestMapping("/quick2")
+    public ModelAndView save2 () {
+        /*
+            model：模型，用于封装数据
+            view：视图，用于展示数据
+         */
+        ModelAndView modelAndView = new ModelAndView();
+        //设置模型数据（相当于放入请求作用域中）
+        modelAndView.addObject("username","gaojie");
+        //设置视图名称
+        modelAndView.setViewName("success");
+        return modelAndView;
     }
 }
