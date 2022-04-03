4e310583e97bbd46b2db8c3dd88983a261a53414
高杰 <gao4433@qq.com>
2022-04-03 23:26:02 +0800

    SpringMVC的数据响应方式之回写数据

--- spring_mvc/src/main/java/cc/gaojie/controller/UserController.java
+++ spring_mvc/src/main/java/cc/gaojie/controller/UserController.java
@@ -1,13 +1,20 @@
 package cc.gaojie.controller;
 
+import cc.gaojie.domain.King;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.xml.ws.RequestWrapper;
+import java.io.IOException;
+import java.nio.file.WatchEvent.Kind;
 
 /**
  * @author gaojie  gao4433@qq.com
@@ -72,4 +79,37 @@ public class UserController {
         request.setAttribute("username","Beautiful!");
      return "success";
      }
+
+     @RequestMapping(value = "/quick6")
+     public void save6 (HttpServletResponse response) throws IOException {
+        response.getWriter().print("Hello World!");
+     }
+
+     @RequestMapping(value = "/quick7")
+     @ResponseBody  //告诉SpringMVC框架不要进行页面跳转，直接数据回写
+     public String save7 () {
+        return "Hello gaojie.cc";
+     }
+
+     @RequestMapping(value = "/quick8")
+     @ResponseBody
+     public String save8 () throws IOException {
+         King king =new King();
+         king.setName("gaojie");
+         king.setAge(22);
+         //使用json转换工具将对象转换为json格式字符串返回
+         ObjectMapper objectMapper = new ObjectMapper();
+         String json = objectMapper.writeValueAsString(king);
+         return json;
+         //return "{\"username\":\"gaojie\",\"age\":22}";
+     }
+
+     @RequestMapping(value = "/quick9")
+     @ResponseBody
+     public King save9 () throws IOException {
+         King king =new King();
+         king.setName("gaojie");
+         return king;
+     }
+
 }
---
+++ spring_mvc/src/main/java/cc/gaojie/domain/King.java
@@ -0,0 +1,28 @@
+package cc.gaojie.domain;
+
+/**
+ * @author gaojie  gao4433@qq.com
+ * @Description
+ * @date 2022/4/3 18:31
+ * @since 1.8
+ */
+public class King {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
