8bf4893e2b9522904294b5e97e37a0ba3618d100
高杰 <gao4433@qq.com>
2022-04-05 21:45:05 +0800

    SpringMVC获取ServletAPI、请求头信息

--- spring_mvc/src/main/java/cc/gaojie/controller/UserController.java
+++ spring_mvc/src/main/java/cc/gaojie/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.xml.ws.RequestWrapper;
 import java.io.IOException;
 import java.nio.file.WatchEvent.Kind;
@@ -169,6 +170,36 @@ public class UserController {
     public void save17(Date date) {
         System.out.println(date);
     }
+
+    @RequestMapping(value = "/quick18")
+    @ResponseBody
+    public void save18(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+        System.out.println(request);
+        System.out.println(response);
+        System.out.println(session);
+    }
+
+    /**
+     * 获取User-Agent
+     * @param user_agent
+     */
+    @RequestMapping(value = "/quick19")
+    @ResponseBody
+    public void save19 (@RequestHeader(value = "User-Agent",required = false) String user_agent) {
+        System.out.println(user_agent);
+    }
+
+    /**
+     * 获取cookie
+     * @param jsessionID
+     */
+    @RequestMapping(value = "/quick20")
+    @ResponseBody
+    public void save20 (@CookieValue(value = "JSESSIONID") String jsessionID) {
+        System.out.println(jsessionID);
+    }
+
+
 
 
 }
