721eaf5e7481c2e16a64280353dfe2f30587c734
高杰 <gao4433@qq.com>
2022-05-13 00:46:21 +0800

    MyBatisMyBatis核心配置文件详解

--- mybatis_quick/src/test/java/cc/gaojie/test/MyBatisTest.java
+++ mybatis_quick/src/test/java/cc/gaojie/test/MyBatisTest.java
@@ -1,5 +1,6 @@
 package cc.gaojie.test;
 
+import cc.gaojie.domain.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -18,6 +19,10 @@ import java.util.List;
  */
 public class MyBatisTest {
 
+    /**
+     * 查询操作
+     * @throws IOException
+     */
     @Test
     public void test1() throws IOException {
         //加载核心配置文件
@@ -30,6 +35,112 @@ public class MyBatisTest {
         List<Object> userList = sqlSession.selectList("userMapper.findAll");
         //打印数据
         System.out.println(userList);
+        //释放资源
+        sqlSession.close();
+    }
+
+    /**
+     * 插入操作
+     * @throws IOException
+     */
+    @Test
+    public void test2() throws IOException {
+
+        //模拟user对象
+        User user = new User();
+        user.setUsername("lisa");
+        user.setPassword("abc");
+
+        //加载核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        //获取session工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        //获得session对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //执行操作   参数：namespace.id
+        sqlSession.insert("userMapper.save",user);
+
+        //提交事务   （MyBatis默认事务是不提交的）
+        sqlSession.commit();
+
+        //释放资源
+        sqlSession.close();
+    }
+
+    /**
+     * 修改操作
+     * @throws IOException
+     */
+    @Test
+    public void test3() throws IOException {
+
+        //模拟user对象
+        User user = new User();
+        user.setId(57);
+        user.setUsername("gaojie");
+        user.setPassword("cc");
+
+        //加载核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        //获取session工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        //获得session对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //执行操作   参数：namespace.id
+        sqlSession.update("userMapper.modify",user);
+
+        //提交事务   （MyBatis默认事务是不提交的）
+        sqlSession.commit();
+
+        //释放资源
+        sqlSession.close();
+    }
+
+    /**
+     * 删除操作
+     * @throws IOException
+     */
+    @Test
+    public void test4() throws IOException {
+
+        //加载核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        //获取session工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        //获得session对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //执行操作   参数：namespace.id
+        sqlSession.delete("userMapper.delete",56);
+
+        //提交事务   （MyBatis默认事务是不提交的）
+        sqlSession.commit();
+
+        //释放资源
+        sqlSession.close();
+    }
+
+    /**
+     * 条件查询操作
+     * @throws IOException
+     */
+    @Test
+    public void test5() throws IOException {
+
+        //加载核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        //获取session工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        //获得session对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //执行操作   参数：namespace.id
+        List<Object> gaojie = sqlSession.selectList("userMapper.selectByName", "gaojie");
+        //打印结果
+        System.out.println(gaojie);
+
         //释放资源
         sqlSession.close();
     }
