a4275b644816195343a287ffd10a93f1c681f982
高杰 <gao4433@qq.com>
2022-06-25 20:13:12 +0800

    一对多关联查询操作之子查询

---
+++ mybatis-incidencemapping/src/main/java/cc/gaojie/dao/StudentDAO.java
@@ -0,0 +1,13 @@
+package cc.gaojie.dao;
+
+import cc.gaojie.pojo.Student;
+
+/**
+ * @author gaojie  gao4433@qq.com
+ * @Description
+ * @date 2022/6/25 19:42
+ * @since 1.8
+ */
+public interface StudentDAO {
+    public Student queryStudentByCid(int cid);
+}
