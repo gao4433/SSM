12de4addd9f21f801b514997e5c370049e738291
高杰 <gao4433@qq.com>
2022-06-26 17:00:06 +0800

    多对一关联查询操作（关联查询、子查询）

---
+++ mybatis-incidencemapping/src/test/java/cc/gaojie/dao/StudentDAOTest.java
@@ -0,0 +1,14 @@
+package cc.gaojie.dao;
+
+import cc.gaojie.pojo.Student;
+import cc.gaojie.utils.MyBatisUtil;
+import junit.framework.TestCase;
+
+public class StudentDAOTest extends TestCase {1
+
+    public void testQueryStudentBySid() {
+        StudentDAO studentDAO = MyBatisUtil.getMapper(StudentDAO.class);
+        Student student = studentDAO.queryStudentBySid("1004");
+        System.out.println(student);
+    }
+}
